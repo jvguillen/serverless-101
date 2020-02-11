adbd641c89e46340743faafa9b95e27c863670cf
Jose Guillen <jvguillen@users.noreply.github.com>
2020-02-11 11:58:09 -0500

    Add goodbye function

--- handler.ts
+++ handler.ts
@@ -10,3 +10,16 @@ export const hello: APIGatewayProxyHandler = async (event, _context) => {
     }, null, 2),
   };
 }
+
+export const goodbye: APIGatewayProxyHandler = async (event, _context) => {
+  const body = event.body ? JSON.parse(event.body) : undefined
+  const name = body.name ? body.name : undefined 
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Goodbye ${name}`,
+      input: event,
+    }, null, 2),
+  };
+}
